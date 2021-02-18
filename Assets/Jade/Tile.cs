246297dc67b21ac11e16a05291ec2e2ecb581a62
oJadeo <jade.jiravate@gmail.com>
2021-02-19 00:58:00 +0700

    map

---
+++ Assets/Jade/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TileMap{
+    public class Tile : MonoBehaviour
+    {
+        public Tile(){
+            
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
