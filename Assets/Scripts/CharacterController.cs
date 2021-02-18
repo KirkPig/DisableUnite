df73a35b542355f6a022ed9580745d97750453dd
NatapongS <natapong099@gmail.com>
2021-02-18 21:32:47 +0700

    First  update

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ICharacter bat;
+    public ICharacter tree;
+    public ICharacter slime;
+    public ICharacter selectedCharacter;
+    public float cooldown;
+    public float cooldownTime;
+    void Start()
+    {
+        cooldown = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown = Mathf.Max(0f, cooldown - Time.deltaTime);
+        if (cooldown > 0f) return; 
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            int x;
+            if (Input.GetAxis("Vertical") < 0)
+            {
+                x = -1;
+            }
+            else x = 1;
+            selectedCharacter.move(new KeyValuePair<int, int>(0, x));
+            cooldown = cooldownTime;
+
+        }
+        else if(Input.GetAxis("Horizontal") !=  0)
+        {
+            int x;
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                x = -1;
+            }
+            else x = 1;
+            selectedCharacter.move(new KeyValuePair<int, int>(x, 0));
+            cooldown = cooldownTime;
+        }
+    }
+    void alarm(KeyValuePair<int, int> newPosition)
+    {
+        slime.move(newPosition);
+    }
+
+}
---
+++ Assets/Scripts/SlimeCharacter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeCharacter : MonoBehaviour, ICharacter
+{
+    KeyValuePair<int, int> position;
+    KeyValuePair<int, int> newPosition;
+    public KeyValuePair<int, int> getPosition()
+    {
+        return position;
+    }
+
+    public void move(KeyValuePair<int, int> direction)
+    {
+        newPosition = position;
+    }
+
+    public void setPosition(int x, int y)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!KeyValuePair<int, int>.Equals(newPosition, position)) 
+        {
+            KeyValuePair<int, int> diff = new KeyValuePair<int, int>( newPosition.Key - position.Key, newPosition.Value - position.Value );
+            if (diff.Key != 0 && diff.Value != 0) MoveTo(new KeyValuePair<int, int>(diff.Key / Mathf.Abs(diff.Key), diff.Value / Mathf.Abs(diff.Value)));
+            else if (diff.Key != 0) MoveTo(new KeyValuePair<int, int>(diff.Key / Mathf.Abs(diff.Key), 0));
+            else MoveTo(new KeyValuePair<int, int>(0, diff.Value / Mathf.Abs(diff.Value)));
+        }
+
+    }
+
+    void MoveTo(KeyValuePair<int, int> diffVector)
+    {
+        position = new KeyValuePair<int, int>(position.Key + diffVector.Key, position.Value + diffVector.Value);
+        //interact with something
+
+        return ;
+    }
+}
