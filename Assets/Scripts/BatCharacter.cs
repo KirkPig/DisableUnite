2521514e91e74bb4dfeda103a370c448897725b7
NatapongS <natapong099@gmail.com>
2021-02-19 19:21:15 +0700

    Make bat able to push plant

--- Assets/Scripts/BatCharacter.cs
+++ Assets/Scripts/BatCharacter.cs
@@ -9,7 +9,32 @@ public class BatCharacter : MonoBehaviour, ICharacter, Interactable
     public StageManager stage;
     public void move(Vector3 newPosition)
     {
-        gameObject.transform.position = newPosition;
+        position = gameObject.transform.position;
+        int x = (int) newPosition.x, y = (int) newPosition.y;
+        Debug.Log(x.ToString() + " " + y.ToString());
+        GameObject inNewPosition = stage.GetMapGameObject(x, y);
+        if(inNewPosition == null)
+        {
+            gameObject.transform.position = newPosition;
+        }
+        else if(inNewPosition.GetComponent<Interactable>() != null) 
+        {
+            Interactable interaction = inNewPosition.GetComponent<Interactable>();
+            Debug.Log("current position:" + position.x.ToString() + " " + position.y.ToString());
+            int result = interaction.interact(true, newPosition - position);
+            if (result == 1)
+            {
+                gameObject.transform.position = newPosition;
+                stage.SetMapGameObject((int)position.x, (int)position.y, null);
+                stage.SetMapGameObject(x, y, gameObject);
+            }
+            else if (result == 3)
+            {
+                //restart
+            }
+         
+        }
+        
     }
     public void getVision()
     {
@@ -30,7 +55,7 @@ public class BatCharacter : MonoBehaviour, ICharacter, Interactable
     //End ICharacter
 
     //begin Interactable
-    public int interact(bool isCharacter, KeyValuePair<int, int> direction)
+    public int interact(bool isCharacter, Vector3 direction)
     {
         return 0;
     }
--- Assets/Scripts/PlantCharacter.cs
+++ Assets/Scripts/PlantCharacter.cs
@@ -2,8 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlantCharacter : MonoBehaviour
+public class PlantCharacter : MonoBehaviour, ICharacter, Interactable
 {
+    Vector3 position;
+    public StageManager stage;
+    //Begin Icharacter
+    public Vector3 getPosition()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void getVision()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void move(Vector3 newPosition)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void setPosition(int x, int y)
+    {
+        throw new System.NotImplementedException();
+    }
+    //Eind ICharacter
+
+    //Begin Interactable
+    public int interact(bool isCharacter, Vector3 direction)
+    {
+        position = gameObject.transform.position;
+        if (!isCharacter) return 2;
+        Vector3 newPosition = position + direction;
+        int x = (int)newPosition.x, y = (int)newPosition.y;
+        if (stage.GetMapGameObject(x, y) == null)
+        {
+            Debug.Log("Still here");
+            Debug.Log(x.ToString() + " " + y.ToString());
+            //stage.SetMapGameObject(x, y, gameObject);
+            gameObject.transform.position = newPosition;
+            return 1;
+        }
+        return 2;
+    }
+    //End Interactable
+
     // Start is called before the first frame update
     void Start()
     {
