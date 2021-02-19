0b49ab97856f157496ae4d8825dbb69545fef97a
KirkPig <kriklomark@gmail.com>
2021-02-19 20:37:28 +0700

    added ReadCSV

---
+++ Assets/Scripts/TestReadCSVScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestReadCSVScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public TextAsset csvFile;
+    void Start()
+    {
+        ReadCSV.ReadCSVFile(csvFile.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
