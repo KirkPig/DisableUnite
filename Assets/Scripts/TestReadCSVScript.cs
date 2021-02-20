7c03b110ec7c9439c78b4851830d0ef94b2d9a3d
KirkPig <kriklomark@gmail.com>
2021-02-20 11:57:08 +0700

    added generate wall

--- Assets/Scripts/TestReadCSVScript.cs
+++ Assets/Scripts/TestReadCSVScript.cs
@@ -7,9 +7,23 @@ public class TestReadCSVScript : MonoBehaviour
     // Start is called before the first frame update
 
     public TextAsset csvFile;
+    public GameObject WallPrefab;
+
     void Start()
     {
-        ReadCSV.ReadCSVFile(csvFile.text);
+        string[,] Map = ReadCSV.ReadCSVFile(csvFile.text);
+
+        for (int i = 0; i < 32; i++)
+        {
+            for (int j = 0;j < 32;j++)
+            {
+                if (Map[i, j] == "3")
+                {
+                    Instantiate(WallPrefab, new Vector3(i, j, transform.position.z), Quaternion.identity);
+                }
+            }
+        }
+
     }
 
     // Update is called once per frame
