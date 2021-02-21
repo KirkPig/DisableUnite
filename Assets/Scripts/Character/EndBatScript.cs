e90487cc1da3af67dd6d843142720470ad8f60fb
NatapongS <natapong099@gmail.com>
2021-02-21 14:30:12 +0700

    Fix conveyer and add end game

---
+++ Assets/Scripts/Character/EndBatScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndBatScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameController stage;
+    public bool isEndBat;
+    void Start()
+    {
+        stage = GameObject.Find("GameController").GetComponent<GameController>();
+        isEndBat = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int x = (int) transform.position.x, y = (int) transform.position.y;
+        GameObject goj = stage.GetMapGameObject(x, y);
+        isEndBat =  (goj != null && goj.GetComponent<BatCharacter>() != null) ;
+    }
+}
