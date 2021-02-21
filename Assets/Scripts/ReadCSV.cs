bc5f77062d03f4477f94ad26df0e39ced8356239
NatapongS <natapong099@gmail.com>
2021-02-21 13:37:43 +0700

    Fix bugs

--- Assets/Scripts/ReadCSV.cs
+++ Assets/Scripts/ReadCSV.cs
@@ -56,8 +56,6 @@ public class ReadCSV
 
         return p;
 
-
-
     }
 
 
@@ -88,31 +86,6 @@ public class ReadCSV
 
 
     }
-    public static string[,] ReadCSVFileNoReverse(string csvText)
-    {
 
-        string[,] p = new string[34, 34];
-
-        string[] lines = csvText.Split("\n"[0]);
-
-        Debug.Log(lines.Length);
-
-        for (int i = 0; i < lines.Length; i++)
-        {
-
-
-            string[] col = lines[i].Split(","[0]);
-
-            for (int j = 0; j < col.Length; j++)
-            {
-                p[j, i] = col[j].Trim();
-            }
-        }
-
-
-        return p;
-
-
-    }
 
 }
